d147ba105af92075ca6b2d878fc37635d670fdc9
NEK0128 <kenrxj@gmail.com>
2014-06-17 22:11:28 +0900

    mod Euclid

--- MyLibraly/Euclidean_distance.py
+++ MyLibraly/Euclidean_distance.py
@@ -1,4 +1,4 @@
-##
+#
 #更新日: 2014/06/16
 #作成者: 松田　健司
 #概要:  ユークリッド距離のクラス
@@ -14,12 +14,13 @@
 import math
 
 class Euclidean_distance:
-	## コンストラクタ
-	def __init__(self, st, go):
-		self.st = st
-		self.go = go
+    ## コンストラクタ
+    def __init__(self, st, go):
+        self.st = st
+        self.go = go
 
-	## 距離計算メソッド
-	def calc_dist(self):
-		dist = math.sqrt((self.st[0] - self.go[0]) ** 2 + (self.st[1] - self.go[1]) ** 2)
-		return dist
+    ## 距離計算メソッド
+    def calc_dist(self):
+        dist = math.sqrt((self.st[0] - self.go[0]) ** 2
+        + (self.st[1] - self.go[1]) ** 2)
+        return dist
